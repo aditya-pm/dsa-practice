c3a3758bfb8a1e7d4a02aa42a18eb50eff6bb485
aditya-pm <adityapmenon29@gmail.com>
2026-01-22 22:19:31 +0530

    Add for loop version of insertion_sort

--- algorithms/insertion_sort.cpp
+++ algorithms/insertion_sort.cpp
@@ -1,9 +1,27 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
 // some people call hole as key too
 
-void insertion_sort(std::vector<int>& arr) {
+void insertion_sort_for(std::vector<int>& arr) {
+    int n = static_cast<int>(arr.size());
+
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int hole;
+        for (hole = i; hole > 0; hole--) {
+            if (arr[hole - 1] > key) {
+                arr[hole] = arr[hole - 1];
+            } else {
+                break;
+            }
+        }
+        arr[hole] = key;
+    }
+}
+
+void insertion_sort_while(std::vector<int>& arr) {
     int n = static_cast<int>(arr.size());
 
     for (int i = 1; i < n; i++) {
@@ -19,7 +37,9 @@ void insertion_sort(std::vector<int>& arr) {
 
 int main() {
     std::vector<int> arr = {7, 2, 4, 1, 5, 3};
-    insertion_sort(arr);
+
+    insertion_sort_for(arr);
+    insertion_sort_while(arr);
 
     for (int x : arr) std::cout << x << " ";
     std::cout << std::endl;

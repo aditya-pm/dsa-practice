b097b0c1d2316464ecd88d426764bc9bac786928
aditya-pm <adityapmenon29@gmail.com>
2026-01-23 20:12:59 +0530

    Add optimized bubble sort

--- algorithms/bubble_sort.cpp
+++ algorithms/bubble_sort.cpp
@@ -13,6 +13,22 @@ void bubble_sort(std::vector<int>& arr) {
     }
 }
 
+// optimized bubble sort for already sorted array
+void bubble_sort_optimized(std::vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 0; i < n - 1; i++) {
+        bool swapped = false;
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                std::swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+
+        if (!swapped) break;
+    }
+}
+
 int main() {
     std::vector<int> arr = {7, 2, 4, 1, 5, 3};
 

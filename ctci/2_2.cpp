f831996b50c60f28f81bad90ddc3f03d31587034
aditya-pm <adityapmenon29@gmail.com>
2026-01-27 10:07:10 +0530

    Add 2 pointer solution (CTCI 2.2)

---
+++ ctci/2_2.cpp
@@ -0,0 +1,37 @@
+#include <LinkedList.h>
+
+ListNode* head = nullptr;
+ListNode* tail = nullptr;
+
+// k = 1 (last node), k = 2 (2nd last node)
+ListNode* kth_last(ListNode* head, int k) {
+    if (head == nullptr || k <= 0) return nullptr;
+
+    ListNode* slow = head;
+    ListNode* fast = head;
+
+    for (int i = 0; i < k; i++) {
+        if (fast == nullptr) return nullptr;
+        fast = fast->next;
+    }
+
+    while (fast != nullptr) {
+        slow = slow->next;
+        fast = fast->next;
+    }
+
+    return slow;
+}
+
+int main() {
+    createNode(1, &head);
+    createNode(2, &head);
+    createNode(3, &head);
+    createNode(4, &head);
+    createNode(5, &head);
+    printList(head);
+
+    std::cout << kth_last(head, 2)->val << std::endl;
+
+    return 0;
+}

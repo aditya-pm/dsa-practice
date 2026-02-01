7c09ad76c203ea448d59c2b9532217b4faaaff97
aditya-pm <adityapmenon29@gmail.com>
2026-02-01 15:07:11 +0530

    Add case 1 recursive solution (CTCI 2.5)

--- ctci/2_5.cpp
+++ ctci/2_5.cpp
@@ -2,6 +2,8 @@
 
 #include "LinkedList.h"
 
+// CASE 1: reversed list
+// iterative
 ListNode* sum_lists(ListNode* l1, ListNode* l2) {
     ListNode dummy(0);
     ListNode* curr = &dummy;
@@ -28,6 +30,23 @@ ListNode* sum_lists(ListNode* l1, ListNode* l2) {
     return dummy.next;
 }
 
+// recursive
+ListNode* sum_lists_recursive(ListNode* l1, ListNode* l2, int carry) {
+    if (!l1 && !l2 && carry == 0) return nullptr;
+
+    int sum = carry;
+    if (l1) sum += l1->val;
+    if (l2) sum += l2->val;
+
+    ListNode* result = new ListNode(sum % 10);
+    result->next = sum_lists_recursive(
+        l1 ? l1->next : nullptr,
+        l2 ? l2->next : nullptr,
+        sum / 10);
+
+    return result;
+}
+
 int main() {
     ListNode* head1 = nullptr;
     ListNode* tail1 = nullptr;
@@ -44,13 +63,16 @@ int main() {
     append_node(2, &head2, &tail2);
 
     print_list(head1);
-    std::cout << "+" << std::endl;
+    std::cout << "+\n";
     print_list(head2);
 
-    std::cout << "sum:" << std::endl;
+    std::cout << "sum (iterative): ";
     ListNode* sum_list = sum_lists(head1, head2);
-
     print_list(sum_list);
+
+    std::cout << "sum (recursive): ";
+    ListNode* sum_list_recursive = sum_lists_recursive(head1, head2, 0);  // initial carry = 0
+    print_list(sum_list_recursive);
 
     return 0;
 }

7e505264a8956cbaa87d766988d62b6d9045c8e3
aditya-pm <adityapmenon29@gmail.com>
2026-02-02 10:36:47 +0530

    Add CodeForces template

---
+++ cf/template.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type aliases (shorter names)
+// using ll = long long;
+// using pii = pair<int, int>;
+// using vi = vector<int>;
+
+// Solve ONE test case
+void solve() {
+    // write solution here
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    cin >> t;  // comment this line if problem has single test case
+    while (t--) {
+        solve();
+    }
+    return 0;
+}

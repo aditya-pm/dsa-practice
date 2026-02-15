b175fc4ecaf50b6c4a5422c4005ab8c2ee0240ae
aditya-pm <adityapmenon29@gmail.com>
2026-02-15 19:57:51 +0530

    Add quick sort algorithm

---
+++ algorithms/quick_sort.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+int partition(std::vector<int>& arr, int start, int end) {
+    int pivot = arr[end];
+    int partition_index = start;
+
+    for (int i = start; i < end; i++) {
+        if (arr[i] <= pivot) {
+            std::swap(arr[i], arr[partition_index]);
+            partition_index++;
+        }
+    }
+
+    std::swap(arr[partition_index], arr[end]);
+
+    return partition_index;
+}
+
+void quick_sort(std::vector<int>& arr, int start, int end) {
+    if (start >= end) return;
+
+    int partition_index = partition(arr, start, end);
+    quick_sort(arr, start, partition_index - 1);
+    quick_sort(arr, partition_index + 1, end);
+}
+
+int main() {
+    std::vector<int> arr = {2, 4, 1, 6, 8, 5, 3, 7};
+    quick_sort(arr, 0, arr.size() - 1);
+
+    for (int x : arr) std::cout << x << " ";
+    std::cout << std::endl;
+
+    return 0;
+}

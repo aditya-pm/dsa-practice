4b12c3648c7e293ec2a1084655617a90961da984
aditya-pm <adityapmenon29@gmail.com>
2026-02-15 20:25:20 +0530

    Add binary search recursive approach

--- algorithms/binary_search.c
+++ algorithms/binary_search.c
@@ -19,9 +19,35 @@ void binary_search(int target, int* arr, int n) {
     printf("Element not found!\n");
 }
 
+void binary_search_recursive(int target, int* arr, int left, int right) {
+    if (left > right) {
+        printf("Element not found!\n");
+        return;
+    }
+
+    int mid = (left + right) / 2;
+
+    if (target == arr[mid]) {
+        printf("Element found at index %d\n", mid);
+        return;
+    } else if (target > arr[mid]) {
+        binary_search_recursive(target, arr, mid + 1, right);
+    } else if (target < arr[mid]) {
+        binary_search_recursive(target, arr, left, mid - 1);
+    }
+}
+
 int main() {
     int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    binary_search(8, arr, 9);
+
+    binary_search(9, arr, 9);
+    binary_search_recursive(9, arr, 0, 8);
+
+    binary_search(1, arr, 9);
+    binary_search_recursive(1, arr, 0, 8);
+
+    binary_search(10, arr, 9);
+    binary_search_recursive(10, arr, 0, 8);
 
     return 0;
 }
